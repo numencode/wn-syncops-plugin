ed35b18a1e118ae0adce2768b60c1028a008b54f
Blaz Orazem <blaz@orazem.si>
2025-09-21 14:40:30 +0200

    Add GitHub workflows

---
+++ tests/BoilerplateTest.php
@@ -0,0 +1,14 @@
+<?php namespace NumenCode\SyncOps\Tests;
+
+use PluginTestCase;
+
+class BoilerplateTest extends PluginTestCase
+{
+    /**
+     * Test boilerplate
+     */
+    public function testBoilerplate(): void
+    {
+        $this->assertEquals(true, true);
+    }
+}
